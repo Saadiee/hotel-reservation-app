669fc6e63f662ff7af9f92fc0bd5585c275e7ece
Saadiee <saad_eew@hotmail.com>
2025-06-15 07:33:10 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="rounded bg-blue-500 p-2 font-bold text-white hover:bg-blue-700">
+      Hello World
+    </div>
+  );
+};
+export default App;
