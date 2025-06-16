5053a7242c10756f175584092169cd5cdc40054a
Saadiee <saad_eew@hotmail.com>
2025-06-15 22:12:42 -0700

    feat: Implement Home page and Hero component; enhance Navbar with user authentication and navigation links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-    </>
-  )
-}
 
-export default App
+  const isOwner = useLocation().pathname.includes("owner")
+  return (
+    <div>
+      {!isOwner && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
