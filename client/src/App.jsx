3f3b4a55d5a7a225e5d9dd29f5da9a636eba36bc
Saadiee <saad_eew@hotmail.com>
2025-06-15 20:36:26 -0700

    feat: Add new assets and implement Navbar component
    - Added various SVG and PNG assets including icons and images for the application.
    - Created a responsive Navbar component with links for navigation.
    - Integrated scroll detection for dynamic styling of the Navbar.
    - Updated index.css to include new font imports and theme variables.
    - Modified main.jsx to wrap the application in BrowserRouter for routing support.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react'
+import Navbar from './components/Navbar'
 
-export const App = () => {
+const App = () => {
   return (
-    <div className="rounded bg-blue-500 p-2 font-bold text-white hover:bg-blue-700">
-      Hello World
-    </div>
-  );
-};
-export default App;
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
